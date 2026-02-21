d18b9fea807bce039dd4f365e33ac1657e480810
voronovslark-lab <voronovslark@gmail.com>
2026-02-21 15:55:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -136,6 +136,6 @@ def webhook():
 def home():
     return "Bot is running"
 
-if name == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
