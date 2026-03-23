5ce292c09b30de685ced4219b24ab8929ad61612
voronovslark-lab <voronovslark@gmail.com>
2026-03-23 03:38:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -107,6 +107,6 @@ def webhook():
 def home():
     return "ok"
 
-if name == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
